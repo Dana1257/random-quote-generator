ffcd1757bcf39704cfaf0ef1ed0d8c7c27e71643
Dana <danahayatt@gmail.com>
2022-10-30 21:33:13 -0400

    Add the variable myQuotes, which includes 8 quotes

---
+++ javascript.js
@@ -0,0 +1,25 @@
+var  myQuotes = [
+"I am free to do as I please as long as I don’t break the law or hurt another person.", 
+"I have every right to believe what I want to believe and live the way that I want to live, but I have a responsibility to respect those who have different beliefs than me and/or live differently than me.", 
+"What’s 100% comfortable for one person can be 100% uncomfortable for someone else.",
+"What’s easy breezy for one person, can be brain surgery for someone else.",
+"It’s a pleasure to be kind to others.",
+"What’s perfectly right for one person can be perfectly wrong for someone else.",
+"I don’t criticize people on their disabilities, I look up to them because of their abilities.",
+"I respect ideas I don’t agree with."
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
